f175e683d4963039d9625c5223b3bf04b1a2976a
Philippe Charrière <ph.charriere@gmail.com>
2024-01-22 05:58:14 +0000

    💾 Saved.

--- 10-go-host-application/build.sh
+++ 10-go-host-application/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # The -ldflags="-s -w" flag is used to optimize the size and reduce the noise of the generated binary. 
-clear
-bat $0 --line-range 5:
-echo ""
 go build -ldflags="-s -w"
 ls -lh hostapp
-
